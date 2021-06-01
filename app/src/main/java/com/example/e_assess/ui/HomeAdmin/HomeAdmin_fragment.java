7dc7e02fd2f97417d712a078776f649b7c7f477e
PalakShravagi <pashk.firebase11@gmail.com>
2021-06-01 17:06:44 +0530

    bottom activity added!

--- app/src/main/java/com/example/e_assess/ui/HomeAdmin/HomeAdmin_fragment.java
+++ app/src/main/java/com/example/e_assess/ui/HomeAdmin/HomeAdmin_fragment.java
@@ -1,6 +1,5 @@
 package com.example.e_assess.ui.HomeAdmin;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,24 +8,24 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.LifecycleOwner;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.example.e_assess.R;
-import com.example.e_assess.ui.home.HomeViewModel;
 
-public class HomeAdmin_fragment extends Fragment {
-    private HomeAdminViewModel homeAdminViewModel;
+public class HomeAdmin_fragment  extends Fragment {
+    private HomeAdminViewModel homeViewModel;  
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeAdminViewModel =
-                new ViewModelProvider((ViewModelStoreOwner) this).get(HomeAdminViewModel.class);
+        homeViewModel =
+                new ViewModelProvider( this).get(HomeAdminViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home_admin, container, false);
         final TextView textView = root.findViewById(R.id.text_home_admin);
-        homeAdminViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -37,4 +36,5 @@ public class HomeAdmin_fragment extends Fragment {
 
 
 
+    
 }
